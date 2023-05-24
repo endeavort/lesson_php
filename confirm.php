d44ec7c3dc1d48cf1054b08f991d3a1fcd4da723
220100 <220100kd@yse-c.net>
2023-05-24 11:53:04 +0900

    add file

---
+++ confirm.php
@@ -0,0 +1,29 @@
+<?php
+$regist = $_POST;
+// var_dump($regist)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=3.0">
+    <title>Regist</title>
+</head>
+<body>
+    <h1>確認画面</h1>
+    <p>この内容で登録してもよろしいですか？</p>
+    <form action="" method="post">
+        <div>
+            <label for="">氏名</label>
+            <?= $regist['name'] ?> 
+        </div>
+        <div>
+            <label for="">Email</label>
+            <?= $regist['email'] ?> 
+        </div>
+        <button>登録</button>
+    </form>
+</body>
+</html>
